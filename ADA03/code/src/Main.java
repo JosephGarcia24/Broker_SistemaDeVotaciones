843dc0a75f8c74c8160f937b76749b8c4747e1c9
KarenCampos842 <142437641+KarenCampos842@users.noreply.github.com>
2025-08-31 19:28:12 -0600

    Add files via upload

---
+++ ADA03/code/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author joseph
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String path = "Input/names.txt";
+        System.out.println("Comenzando la lectura del archivo..");
+        ArrayList<String> namesList = FileTextReader.readFile(path); //Se obtiene los datos del archivo de texto y se pasan a un ArrayList
+        System.out.println("\nLista original:");
+        
+        for(String name: namesList)
+            System.out.println(name);
+        
+        System.out.println("\nFormateando nombres");
+        System.out.println("\nLista formateada:");
+        namesList = TextFormatter.formatNames(namesList); //Se formatea la lista de nombres y se ordena
+        Printer.printArrayList(namesList); //Se imprimen los datos en consola 
+
+     
+    }
+    
+}
