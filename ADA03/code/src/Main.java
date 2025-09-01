a15ccf20dde29aac5371c7a31dc515997bddf6fd
KarenCampos842 <142437641+KarenCampos842@users.noreply.github.com>
2025-08-31 18:14:11 -0600

    Update Main.java

--- ADA03/code/src/Main.java
+++ ADA03/code/src/Main.java
@@ -19,10 +19,10 @@ public class Main {
         System.out.println("Comenzando la lectura del archivo..");
         ArrayList<String> namesList = FileTextReader.readFile(path); //Se obtiene los datos del archivo de texto y se pasan a un ArrayList
         System.out.println("\nLista original:");
+        
         for(String name: namesList)
-        {
             System.out.println(name);
-        }
+        
         System.out.println("\nFormateando nombres");
         System.out.println("\nLista formateada:");
         namesList = TextFormatter.formatNames(namesList); //Se formatea la lista de nombres y se ordena
